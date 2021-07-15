93d772063a400a8d1b459f581fac72402e479e78
jesse <jesse@JESSE-ALIENWARE>
2021-07-15 11:25:58 +0800

    Add project files.

---
+++ ImportAirfoilSketch/AirfoilSketch.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AirfoilSketch
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Cooper Black", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(110, 19)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Airfoil Type"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(128, 17)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(237, 23)
+        Me.ComboBox1.Sorted = True
+        Me.ComboBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.Font = New System.Drawing.Font("Constantia", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Button1.Location = New System.Drawing.Point(371, 9)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(83, 34)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Import" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'AirfoilSketch
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(461, 323)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "AirfoilSketch"
+        Me.Text = "Airfoil Sketch"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ ImportAirfoilSketch/AirfoilSketch.vb
@@ -0,0 +1,124 @@
+﻿Public Class AirfoilSketch
+    Private Structure AirFoilData
+        Public SketchName As String
+        Public FullPath_Sketch As String
+        Public Airfoil_Coord() As Coordinate
+    End Structure
+
+    Private Structure Coordinate
+        Public X_Coordinate As Double
+        Public Y_Coordinate As Double
+    End Structure
+    Private Airfoil As AirFoilData
+    Public CATIAFactory As CATIA_Property = New CATIA_Property
+    Private AirfoilLocation As String
+    Private oPart As MECMOD.Part
+    Private oPartDoc As MECMOD.PartDocument
+    Private Sub CATMain()
+        oPartDoc = CATIAFactory.MyCATIA.ActiveDocument
+        oPart = oPartDoc.Part
+        Dim Airfoil_Geo As MECMOD.HybridBody
+        Try
+            Airfoil_Geo = oPart.HybridBodies.Item("Airfoil Type")
+        Catch ex As Exception
+            Airfoil_Geo = oPart.HybridBodies.Add
+            Airfoil_Geo.Name = "Airfoil Type"
+        End Try
+        Dim Sketch_Airfoil As MECMOD.Sketch
+        Try
+            Sketch_Airfoil = Airfoil_Geo.HybridSketches.Item(Airfoil.SketchName)
+        Catch ex As Exception
+            Sketch_Airfoil = Airfoil_Geo.HybridSketches.Add(oPart.OriginElements.PlaneYZ)
+            Sketch_Airfoil.Name = Airfoil.SketchName
+            Call DrawPoints(Sketch_Airfoil)
+
+        End Try
+
+
+
+
+        oPart.Update()
+    End Sub
+    Private Sub DrawPoints(ByRef oSketch As MECMOD.Sketch)
+        Dim oPoints() As MECMOD.Point2D
+        Dim oCPoints()
+        Dim oSpline As MECMOD.Spline2D
+        ReDim oPoints(0)
+        ReDim oCPoints(0)
+        oSketch.OpenEdition()
+        For i = 1 To UBound(Airfoil.Airfoil_Coord)
+            ReDim Preserve oPoints(UBound(oPoints) + 1)
+            ReDim Preserve oCPoints(UBound(oPoints) + 1)
+            oPoints(UBound(oPoints)) = oSketch.Factory2D.CreatePoint(Airfoil.Airfoil_Coord(i).X_Coordinate * 100, Airfoil.Airfoil_Coord(i).Y_Coordinate * 100)
+            oCPoints(UBound(oCPoints)) = oSketch.Factory2D.CreateControlPoint(Airfoil.Airfoil_Coord(i).X_Coordinate * 100, Airfoil.Airfoil_Coord(i).Y_Coordinate * 100)
+        Next
+        oSpline = oSketch.Factory2D.CreateSpline(oCPoints)
+        oSketch.CloseEdition()
+    End Sub
+    Private Sub AirfoilSketch_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim oFile As String
+        AirfoilLocation = "D:\Work\Matlab\Airfoil_Analyzer\Airfoil_Coordinate_Files"
+        For Each FileN As String In My.Computer.FileSystem.GetFiles(AirfoilLocation, FileIO.SearchOption.SearchAllSubDirectories, "*.dat")
+            Dim FileN_Short As String = System.IO.Path.GetFileNameWithoutExtension(FileN)
+            oFile = FileN
+            ComboBox1.Items.Add(UCase(FileN_Short))
+        Next
+        CATIAFactory = CATIA_Property.SetInitialCATIA()
+        'Call CATMain()
+        'Call GetAirfoilType(oFile)
+    End Sub
+    Private Sub GetAirfoilType(FileN As String)
+
+
+        Dim oFileInfo As System.IO.FileInfo
+        Dim oStreamReader As System.IO.StreamReader
+        Dim ErrorLogList As ArrayList
+        Dim counterFileLine As Integer = 1
+        Dim oArray(0) As Double
+
+        oFileInfo = New IO.FileInfo(FileN)
+        oStreamReader = oFileInfo.OpenText
+        Dim SplitText() As String
+        Dim Text As String = oStreamReader.ReadLine
+
+        Try
+            While Not Text Is Nothing
+                If Text.Trim <> "" Then
+                    SplitText = Text.Split(" ")
+                    For i = 0 To UBound(SplitText)
+                        If IsNumeric(SplitText(i)) = True Then
+                            ReDim Preserve oArray(UBound(oArray) + 1)
+                            oArray(UBound(oArray)) = SplitText(i)
+                        End If
+                    Next
+                End If
+                Text = oStreamReader.ReadLine
+            End While
+
+            'MsgBox(UBound(oArray).ToString)
+            oStreamReader.Close()
+            oStreamReader = Nothing
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        ReDim Airfoil.Airfoil_Coord(0)
+        For i = 1 To UBound(oArray) Step 2
+            ReDim Preserve Airfoil.Airfoil_Coord(UBound(Airfoil.Airfoil_Coord) + 1)
+            Airfoil.Airfoil_Coord(UBound(Airfoil.Airfoil_Coord)).X_Coordinate = oArray(i)
+            Airfoil.Airfoil_Coord(UBound(Airfoil.Airfoil_Coord)).Y_Coordinate = oArray(i + 1)
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Airfoil.SketchName = ComboBox1.SelectedItem
+        For Each FileN As String In My.Computer.FileSystem.GetFiles(AirfoilLocation, FileIO.SearchOption.SearchAllSubDirectories, "*.dat")
+            Dim FileN_Short As String = System.IO.Path.GetFileNameWithoutExtension(FileN)
+            If UCase(FileN_Short) = UCase(Airfoil.SketchName) Then
+                Airfoil.FullPath_Sketch = FileN
+                Exit For
+            End If
+        Next
+        Call GetAirfoilType(Airfoil.FullPath_Sketch)
+        Call CATMain()
+    End Sub
+End Class
